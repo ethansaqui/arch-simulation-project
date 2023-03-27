34d25025b91ad1e8a04e2b4066b9a8d48270fc42
Sakui <ethan_saquilayan@dlsu.edu.ph>
2023-03-27 12:49:32 +0800

    chore: add NaN option to select

--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -2,12 +2,21 @@
 
 const baseSelect = document.getElementById('baseSelect') as HTMLInputElement;
 const binaryInput = document.getElementById('binaryInput') as HTMLInputElement;
+const exponentInput = document.getElementById('exponentInput') as HTMLInputElement;
 
 baseSelect.addEventListener('change', handleBaseSelectChange);
 
 function handleBaseSelectChange() {
+    binaryInput.disabled = false;
+    exponentInput.disabled = false;
     if(baseSelect.value == '2')
         binaryInput.placeholder="Enter a binary mantissa"
-    else
+    else if(baseSelect.value == '10')
         binaryInput.placeholder="Enter a decimal mantissa"
+    else {
+        binaryInput.placeholder="NaN"
+        binaryInput.disabled = true;
+        exponentInput.disabled = true;
+    }
+        
 }
