380f37cc5a26659588d51a758fb2aaec3ca51d3e
Sakui <ethan_saquilayan@dlsu.edu.ph>
2023-04-16 00:56:53 +0800

    deployment build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.html", "./dist/public/scripts/**/*.js"],
+  content: [".//*.html", "./dist/public/scripts/**/*.js"],
   theme: {
     extend: {},
   },
